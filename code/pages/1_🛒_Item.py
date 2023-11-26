00e0bd46977cbcb0f2d1df5f957d63ed8e96b1b6
sebelaspro11 <132761130+sebelaspro11@users.noreply.github.com>
2023-11-27 00:00:39 +0900

    Update 1_🛒_Item.py

--- code/pages/1_🛒_Item.py
+++ code/pages/1_🛒_Item.py
@@ -85,7 +85,7 @@ for selected_item in item_filter:
             x='Premis',
             y='Harga (RM)',
             labels={'Item': 'Item', 'Tarikh': 'Tarikh', 'Harga (RM)': 'Harga (RM)', 'Unit': 'Unit', 'Premis': 'Premis'},
-            title=f'Harga (RM) Semasa {unit} {selected_item} [{latest_date}]',
+            title=f'Harga Semasa {unit} {selected_item} [{latest_date}]',
             text='Harga (RM)',
             hover_data={'Item': True, 'Tarikh': True, 'Harga (RM)': ':.2f', 'Unit': True, 'Premis': True}
         )
@@ -101,7 +101,7 @@ for selected_item in item_filter:
             y='Harga (RM)',
             color='Premis',
             labels={'Item': 'Item', 'Tarikh': 'Tarikh', 'Harga (RM)': 'Harga (RM)', 'Unit': 'Unit', 'Premis': 'Premis'},
-            title=f'Perubahan Harga (RM) Bagi {unit} {selected_item} Pada Bulan {current_month}',
+            title=f'Perubahan Harga Bagi {unit} {selected_item} Pada Bulan {current_month}',
             hover_data={'Item': True, 'Tarikh': True, 'Harga (RM)': ':.2f', 'Unit': True, 'Premis': True}
         )
         st.plotly_chart(fig_line, use_container_width=True)
